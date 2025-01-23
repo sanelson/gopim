a1286f7130234a47b69e6b8850b63482d8132a00
Sam Nelson <sanelson@siliconfuture.net>
2025-01-23 12:09:36 -0700

    Rename for brevity

--- pim.go
+++ pim.go
@@ -28,11 +28,11 @@ var (
 )
 
 func readConfig() {
-	viper.SetConfigName("gopim") // name of config file (without extension)
-	viper.SetConfigType("toml")  // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath(".")     // optionally look for config in the working directory
-	err := viper.ReadInConfig()  // Find and read the config file
-	if err != nil {              // Handle errors reading the config file
+	viper.SetConfigName("pim")  // name of config file (without extension)
+	viper.SetConfigType("toml") // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")    // optionally look for config in the working directory
+	err := viper.ReadInConfig() // Find and read the config file
+	if err != nil {             // Handle errors reading the config file
 		slog.Error("config file issue", "error", err)
 		os.Exit(1)
 	}
