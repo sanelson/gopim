8e2db59a76182b57b3e4a4a52f3f1c2a680cc767
Sam Nelson <sanelson@siliconfuture.net>
2025-02-08 06:46:15 -0700

    Removed az cli support and transitioned to azidentity module

--- pim.go
+++ pim.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -9,8 +10,6 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
-	"os/exec"
-	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -18,6 +17,9 @@ import (
 	"github.com/google/uuid"
 	"github.com/lmittmann/tint"
 	"github.com/spf13/viper"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 )
 
 func readConfig() {
@@ -38,130 +40,6 @@ func contains(slice []string, item string) bool {
 		}
 	}
 	return false
-}
-
-type CommandError struct {
-	Err    error
-	Stderr string
-}
-
-func (e *CommandError) LogError() {
-	//return fmt.Sprintf("command failed: %v\nstderr: %s", e.Err, e.Stderr)
-	slog.Error("command failed:", "error", e.Err, "stderr", e.Stderr)
-}
-
-func runCommand(cmd string, env []string) (string, *CommandError) {
-	slog.Debug("Running command:", "cmd", cmd)
-
-	var command *exec.Cmd
-	if runtime.GOOS == "windows" {
-		command = exec.Command("powershell", "-nologo", "-noprofile", cmd)
-	} else {
-		command = exec.Command("sh", "-c", cmd)
-	}
-
-	command.Env = append(os.Environ(), env...)
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	command.Stdout = &out
-	command.Stderr = &stderr
-	err := command.Run()
-	if err != nil {
-		cmdErr := &CommandError{
-			Err:    err,
-			Stderr: stderr.String(),
-		}
-		return "", cmdErr
-	} else {
-		slog.Debug("Successfully ran command")
-		return out.String(), nil
-	}
-}
-
-func azureCLIVersion() (string, *CommandError) {
-	escape_quote := `"` // escape double quotes for bash
-
-	if runtime.GOOS == "windows" {
-		escape_quote = "`\"" // escape double quotes for PowerShell
-	}
-
-	version, cmdErr := runCommand("az version --output json --query '"+escape_quote+"azure-cli"+escape_quote+"'", nil)
-
-	if cmdErr != nil {
-		return "", cmdErr
-	} else {
-		// Clean up version
-		version = strings.Trim(strings.TrimSpace(version), "\"")
-		return version, nil
-	}
-}
-
-func azureCLIInstalled() bool {
-	// check if Azure CLI is installed
-	version, cmdErr := azureCLIVersion()
-
-	if cmdErr != nil {
-		cmdErr.LogError()
-		slog.Error("Azure CLI not installed or not in path")
-		slog.Info("Install from https://learn.microsoft.com/en-us/cli/azure/install-azure-cli")
-		os.Exit(1)
-	}
-
-	slog.Debug("Azure CLI is installed", "version", version)
-	return true
-}
-
-func azureLoggedIn() bool {
-	_, cmdErr := runCommand("az account show", nil)
-
-	if cmdErr != nil {
-		// We don't need to log the error here unless the command failed for some other reason
-		// Check the error message to see if it's a login error
-		if strings.Contains(cmdErr.Stderr, "Please run 'az login' to setup account") {
-			return false
-		} else {
-			cmdErr.LogError()
-			slog.Error("Failed to check login status")
-			os.Exit(1)
-		}
-	}
-	return true
-}
-
-func getAccessToken() (string, error) {
-	token, cmdErr := runCommand("az account get-access-token --query accessToken --output tsv", nil)
-
-	if cmdErr != nil {
-		cmdErr.LogError()
-		return "", fmt.Errorf("Failed to get access token: %w", cmdErr.Err)
-	}
-	return token, nil
-}
-
-func loginToAzure(tenant string) error {
-	if runtime.GOOS == "windows" {
-		// Open the login URL in the default web browser vs using WAM (Windows Authentication Manager)
-		// WAM behavior is still rather glitchy
-		// TODO: Is there a way to just set this for the current session/process?
-		_, cmdErr := runCommand("az config set core.enable_broker_on_windows=false", nil)
-
-		if cmdErr != nil {
-			cmdErr.LogError()
-			slog.Error("failed to set Azure CLI config to disable WAM login")
-			os.Exit(1)
-		}
-	}
-
-	_, cmdErr := runCommand(fmt.Sprintf("az login --allow-no-subscriptions -t '%s'", tenant), nil)
-
-	if cmdErr != nil {
-		cmdErr.LogError()
-		slog.Error("failed to login to Azure")
-		os.Exit(1)
-	}
-
-	slog.Info("A web browser has been opened at https://login.microsoftonline.com. Please continue the login in the web browser. If no web browser is available or if the web browser fails to open, use device code flow with `az login --use-device-code`.")
-	return nil
 }
 
 func prepareRequest(method, url, token string) (*http.Request, error) {
@@ -273,9 +151,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Check if Azure CLI is installed
-	_ = azureCLIInstalled()
-
 	// read the config file
 	readConfig()
 
@@ -290,26 +165,29 @@ func main() {
 		}
 	}
 
-	if azureLoggedIn() {
-		slog.Info("Already logged in to Azure. Skipping login step.")
-	} else {
-		slog.Info("Not logged in to Azure. Logging in.")
-		_ = loginToAzure(*tenant)
+	credentialOptions := azidentity.InteractiveBrowserCredentialOptions{
+		TenantID: *tenant,
 	}
 
-	// Get the access token
-	token, err := getAccessToken()
+	cred, err := azidentity.NewInteractiveBrowserCredential(&credentialOptions)
+	if err != nil {
+		slog.Error("Failed to login to Azure", "error", err)
+		os.Exit(1)
+	}
+
+	// Create an HTTP client with a 30s timeout
+	context := context.Background()
+	client := &http.Client{Timeout: 30 * time.Second}
+	token, err := cred.GetToken(context, policy.TokenRequestOptions{Scopes: []string{"https://management.azure.com/.default"}})
+
 	if err != nil {
 		slog.Error("Failed to get access token", "error", err)
 		os.Exit(1)
 	}
 
-	// Create an HTTP client with a 30s timeout
-	client := &http.Client{Timeout: 30 * time.Second}
-
 	// Download the role eligibility schedule instances
 	subscriptions := strings.Split(*subs, ",")
-	roles, err := getRESIs(subscriptions, token, *client)
+	roles, err := getRESIs(subscriptions, token.Token, *client)
 
 	if err != nil {
 		slog.Error("Failed to get role eligibility schedule instances", "error", err)
