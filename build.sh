4a9e1447f0d014cafbd39edee403370c7ed5d090
Sam Nelson <sanelson@siliconfuture.net>
2025-02-08 11:10:43 -0700

    Disable default cross-compile for OSX and use MacOS github runner for automated builds

--- build.sh
+++ build.sh
@@ -2,25 +2,48 @@
 
 package=$1
 if [[ -z "$package" ]]; then
-  echo "usage: $0 <package-name>"
+  echo "usage: $0 <package-name> <platform-name>"
   exit 1
 fi
+platform_name=$2
 package_split=(${package//\// })
 package_name=${package_split[-1]}
 	
 platforms=("windows/amd64" "windows/386" "darwin/amd64" "linux/amd64")
 
+# Default platform is current platform
+# Otherwise, build for all platforms if "all" is specified
+if [[ -z "$platform_name" ]]; then
+  platform_name="$(go env GOOS)/$(go env GOARCH)"
+elif [[ "$platform_name" == "darwin/amd64" ]]; then
+  if [[ $(go env GOOS) != "darwin" ]]; then
+    echo "Cross-compiling for darwin/amd64 is no longer supported"
+	exit 1
+  fi
+fi
+
 for platform in "${platforms[@]}"
 do
+	# Check if platform is specified
+	if [[ "$platform_name" != "all" && "$platform" != "$platform_name" ]]; then
+		continue
+	fi
 	platform_split=(${platform//\// })
 	GOOS=${platform_split[0]}
 	GOARCH=${platform_split[1]}
+	CGO_ENABLED=0
 	output_name=$package_name'-'$GOOS'-'$GOARCH
 	if [ $GOOS = "windows" ]; then
 		output_name+='.exe'
-	fi	
+	elif [ $GOOS = "darwin" ]; then
+		# Enable CGO for darwin/amd64 and only try to compile all platforms if on OSX
+  		if [[ $(go env GOOS) != "darwin" ]]; then
+			continue
+		fi
+		CGO_ENABLED=1
+	fi
 
-	env GOOS=$GOOS GOARCH=$GOARCH go build \
+	env CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH go build \
 	  -ldflags="-X main.version=$(git describe --tags)" \
 	  -o $output_name $package
 	if [ $? -ne 0 ]; then
